1f9e9d86b7fb1e9c6c47bfacc208f9b37247ff65
Graylen <delilhabigelow@gmail.com>
2026-02-23 17:11:26 -0500

    [Commit #3] -- Sidebar functionality, style, and responsiveness.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "./components/sidebar";
 import { Route, Routes } from "react-router-dom";
 import ChatBox from "./components/chat-box";
 import Login from "./pages/login";
 import Credits from "./pages/credits";
 import Community from "./pages/community";
+import { assets } from "./assets/assets";
 
 const App = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <>
+      {!isMenuOpen && (
+        <img
+          src={assets.menu_icon}
+          className="absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden not-dark:invert"
+          onClick={() => {
+            setIsMenuOpen(true);
+          }}
+          alt=""
+        />
+      )}
+
       <div className="dark:bg-linear-to-b from-[#242124] to-[#000000] dark:text-white">
         <div className="flex h-screen w-screen">
-          <Sidebar />
+          <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
           <Routes>
             <Route path="/" element={<ChatBox />} />
             <Route path="/credits" element={<Credits />} />
